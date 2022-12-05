cdb1be3b844adc2c430faca79ddd2b720793fd02
HannahMulligan-hm <hannahx08@comcast.net>
2022-12-05 12:52:00 -0500

    sum+avg

---
+++ multiples.py
@@ -0,0 +1,26 @@
+# Multiples
+# # Part I: printing odd numbers
+# # for i in range (1001):
+# # 	if i  %2 == 0:
+# # 		continue
+# # 	else:
+# # 		print i
+
+# # Part II: printing multiples of 5
+# # for i in range (1000000):
+# # 	if i %5 == 0:
+# # 		print i 
+# # 	else:
+# # 		continue
+
+# # Sum List
+a = [1, 2, 5, 10, 255, 3]
+x = 0
+for i in a:
+	x += i
+print x
+y = x/len(a)
+print y
+
+
+
