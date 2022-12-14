6f59510087a0849cdf73f8ddab9c3ff076c4286d
Hannah Mulligan <81582747+HannahMulligan-hm@users.noreply.github.com>
2022-12-14 16:11:48 -0500

    Add files via upload

---
+++ findcharacters.py
@@ -0,0 +1,16 @@
+# # input
+# word_list = ['hello','world','my','name','is','Anna']
+# char = 'o'
+# # output
+# # new_list = ['hello','world']
+# new_list = []
+
+# for word in word_list:
+# 	for w in word:
+# 		if w == char:
+# 			new_list.append(word)
+# 		else: 
+# 			continue
+# print new_list
+
+
